7c3c8ceb66f6268de5a7c73f7ff1a3219b1a8c05
Faruk Aydın <hayspat@gmail.com>
2020-07-08 16:58:38 +0300

    fix auth

--- src/Util/Api.ts
+++ src/Util/Api.ts
@@ -1,14 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { OperationResultDTO, RequestParams } from 'Redux/Helpers/ApiTypes';
 import { Api } from 'Redux/Helpers/ApiTypes';
+import { getHeadersForFetch } from 'Util/Util';
 
 export const GeneratedApi = new Api({
   baseUrl: process.env.REACT_APP_BACKEND_API_URL,
-  baseApiParams: {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  },
+  baseApiParams: getHeadersForFetch(),
 });
 
 export const generateThunk = <RequestDTO, ResponseDTO extends OperationResultDTO>(
@@ -23,12 +20,8 @@ export const generateThunk = <RequestDTO, ResponseDTO extends OperationResultDTO
     }
   >(actionType, async (data, thunkAPI) => {
     try {
-      const response = await fetchFn(data, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      });
+      const response = await fetchFn(data, getHeadersForFetch());
+
       if (!response.result) {
         return thunkAPI.rejectWithValue(response);
       }
